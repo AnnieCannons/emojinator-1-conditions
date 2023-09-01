218ad7194419a22f314c9e7abbce94a9dbb2f024
[Hailey G] <[haileydgrace@gmail.com]>
2023-08-31 21:48:22 -0400

    Essentials Complete

--- interface.js
+++ interface.js
@@ -1,1 +1,4 @@
 // Add your interface code below!
+let question = prompt('Type a one worded emotion here and check the console log to see what you get!\n(Hint: the more creative you are, the more detailed your results are)')
+
+let response = console.log(`%c ${getEmoji(question)} `,`font-size: 40px; font-weight: bold; color: black; text-shadow: 0 0 3px yellow, 0 0 6px yellow, 0 0 9px yellow; background: black;`);
--- logic.js
+++ logic.js
@@ -1,1 +1,23 @@
 // Add your business logic functions below!
+/*
+else if (input.toLowerCase() === ''){
+    return ''
+    }
+*/
+function getEmoji(input) {
+    if (input.toLowerCase() === 'smile') {
+    return '☺';
+    } else if (input.toLowerCase() === 'frown') {
+    return '☹';
+    } else if (input.toLowerCase() === 'heart'){
+        return '❤';
+        } else if (input.toLowerCase() === 'star'){
+            return '⭐';
+            } else if (input.toLowerCase() === 'spinosaur'){
+                return null;
+                } else if (input.toLowerCase() === 'robo t-rex'){
+                    return null;
+                } else {
+                    return "Either that wasn't english or your SAT word outsmarted me!";
+                }
+}
