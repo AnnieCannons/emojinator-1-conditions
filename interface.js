6f7c47acffc4bcc38a308823ba0f39406a536125
[Hailey G] <[haileydgrace@gmail.com]>
2023-09-01 01:31:57 -0400

    Answers - interactive html page for fun

--- interface.js
+++ interface.js
@@ -1,4 +1,28 @@
 // Add your interface code below!
-let question = prompt('Type a one worded emotion here and check the console log to see what you get!\n(Hint: the more creative you are, the more detailed your results are)')
+//let question = prompt('Type a one worded emotion here and check the console log to see what you get!\n(Hint: the more creative you are, the more detailed your results are)')
 
-let response = console.log(`%c ${getEmoji(question)} `,`font-size: 40px; font-weight: bold; color: black; text-shadow: 0 0 3px yellow, 0 0 6px yellow, 0 0 9px yellow; background: black;`);
+
+const style = `
+font-size: 120px; 
+font-weight: bold; 
+color: black; 
+text-shadow: 0 0 3px yellow, 0 0 6px yellow, 0 0 9px yellow, 0 0 15px white, 0 0 20px white; 
+background-image: linear-gradient(to right bottom, #053713, #20611f, #448e25, #72bc23, #a8eb12);`
+
+const errorFormat = `
+font-size: 50px;
+color: black;`
+
+function results() {
+    var input = prompt('Type a one worded emoji here and see what you get!\n(Hint: the more creative you are, the more detailed your results are)');
+    let result = getEmoji(input);
+
+    if ((result === 'undefined') || (result === 'error')) {
+        alert(`🧐 either that wasn't english or I need to expand my vocabulary...`);
+        document.getElementById("answer").innerHTML = `🙊?`
+    }
+    else {
+        document.getElementById("answer").innerHTML = result;
+        console.log(`%c${result}`, style)
+    }
+}
--- logic.js
+++ logic.js
@@ -1,23 +1,30 @@
 // Add your business logic functions below!
 /*
 else if (input.toLowerCase() === ''){
-    return ''
+    return alert(''), console.log(`%c `, style), document.write(getById
     }
 */
+
+
 function getEmoji(input) {
     if (input.toLowerCase() === 'smile') {
-    return '☺';
+        return '☺';
     } else if (input.toLowerCase() === 'frown') {
-    return '☹';
-    } else if (input.toLowerCase() === 'heart'){
+        return '☹';
+    } else if (input.toLowerCase() === 'heart') {
         return '❤';
-        } else if (input.toLowerCase() === 'star'){
-            return '⭐';
-            } else if (input.toLowerCase() === 'spinosaur'){
-                return null;
-                } else if (input.toLowerCase() === 'robo t-rex'){
-                    return null;
-                } else {
-                    return "Either that wasn't english or your SAT word outsmarted me!";
-                }
+    } else if (input.toLowerCase() === 'star') {
+        return '⭐';
+    } else if (input.toLowerCase() === 'spinosaur') {
+        return null;
+    } else if (input.toLowerCase() === 'robo t-rex') {
+        return null;
+    } else if ((input.toLowerCase() === 'stressed') || (input.toLowerCase() === 'overwhelmed') || (input.toLowerCase() === 'maxed')) {
+        return '🫠'
+    }
+    else {
+        return `error`;
+    }
 }
+
+
