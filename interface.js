e7fd724c3059ebbf5835e512bb46b53e3900f7dd
lai_luv <l.lavaughn319@gmail.com>
2023-09-01 21:36:25 -0400

    add logic js to html add functiont to logic js add interface js to html add code to file added more emoji to js file

--- interface.js
+++ interface.js
@@ -1,1 +1,9 @@
 // Add your interface code below!
+let userEmoji = prompt('describe your emoji . ')
+let emoji = getEmoji(userEmoji)
+ 
+if (emoji === null){
+    console.log("Sorry we don't have an emoji matching this description. " + userEmoji )
+ }else{
+    console.log("you picked:" + emoji)
+ }
--- logic.js
+++ logic.js
@@ -1,1 +1,31 @@
 // Add your business logic functions below!
+function getEmoji(emojiType){
+    if (emojiType === "smile"){
+        return "☺"
+    }else if (emojiType === "frown"){
+        return "☹"
+    }else if (emojiType === "heart"){
+        return "❤"
+    }else if (emojiType === "star"){
+        return "⭐"
+    }else if ( emojiType === "money"){
+        return "💰"
+    }else if ( (emojiType === "love")||(emojiType === "heart eyes") ){
+        return "😍"
+    }else if ( emojiType === "loved"){
+        return "🥰"
+    }else if ( (emojiType === "heart kiss face") || (emojiType === "kiss")){
+        return "😘"
+    }else if ( emojiType === "Monkey"){
+        return "🐵"
+    }else if ( emojiType === "lips"){
+        return "🫦"
+    }else if ( (emojiType === " cold") || (emojiType === " freezing "))
+    {
+        return "🥶"
+    }else if ((emojiType === "hot") || (emojiType === "burning ")){
+        return "🥵"
+    }else{
+        return null;
+    }
+}
