c4d96308a3014782ad7d021a1c1ee7c0f547366d
Taylor <taylorlange95@gmail.com>
2023-09-05 11:42:45 -0500

    created emojinator.

--- interface.js
+++ interface.js
@@ -1,1 +1,12 @@
 // Add your interface code below!
+let userInput = prompt("Enter an emoji name.")
+
+
+
+let emoji =getEmoji(userInput);
+
+if (emoji ===null) 
+
+{ console.log(`${userInput} is not a valid search term. The Emojinator currently recognizes the following words: smile, smiley, happy, frown, frowny, sad, star, sparkle, shiny, heart, love `)} else {
+    console.log("Your emoji is " + emoji)
+};
--- logic.js
+++ logic.js
@@ -1,1 +1,16 @@
 // Add your business logic functions below!
+
+
+
+function getEmoji(input) {
+if ((input === "smile") || (input === "smiley") || (input == "happy")) {
+return "☺"
+} else if ((input === "frown") || (input === "frowny") || (input === "sad")) {
+    return "☹"
+} else if ((input === "star") || (input === "sparkle") || (input === "shiny")) {
+    return "⭐"
+} else if ((input === "heart") || (input === "love")) {
+    return "❤"
+} else {
+    return null}
+}
