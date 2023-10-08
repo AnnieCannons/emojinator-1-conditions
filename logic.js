6183f3c2b2c0547ee02ec7f76f5d61769f37d015
Stay-Hack <staysha.l.hackmann@gmail.com>
2023-10-08 07:17:25 -0700

    complete

--- logic.js
+++ logic.js
@@ -1,1 +1,16 @@
 // Add your business logic functions below!
+
+
+function getEmoji(stringFromUser) {
+    if(string === "smile") {
+        return "☺"
+    } else if(string === "frown") {
+        return "☹"
+    } else if(string === "heart") {
+        return "❤"
+    } else if(string === "star") {
+        return "⭐"
+    } else 
+        return null
+}
+getEmoji(stringFromUser)
